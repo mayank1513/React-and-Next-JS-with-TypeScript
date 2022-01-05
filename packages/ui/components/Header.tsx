7368061bed7cb7535807fcc555071b4876e0ae54
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-05 12:43:51 +0530

    Create Todo List App

---
+++ packages/ui/components/Header.tsx
@@ -0,0 +1,72 @@
+import styled from '@emotion/styled'
+import { Button, Logo } from '.'
+import { useRef, useState, useEffect } from 'react'
+
+const HeaderContainer = styled.div/*css*/`
+// background: #87ceeb15;
+max-width: 100vw;
+header {
+    margin: auto;
+    align-items: center;
+    display: flex;
+    padding: 10px;
+    justify-content: space-between;
+    line-height: 1.5;
+}
+`
+
+const FixedHeader = styled.div/*css */`
+max-width: 100vw;
+position: fixed;
+z-index: 1000;
+top: 0;
+left:0;
+right: 0;
+background: white;
+transition: all .5s;
+`
+
+export interface HeaderProps {
+    title: string,
+    ref?: any
+}
+
+function BaseHeader({ title, ref }: HeaderProps) {
+    return (
+        <HeaderContainer ref={ref}>
+            <header>
+                <Logo></Logo>
+                <h1>{title}</h1>
+                <Button style={{ '--bg-color': 'blue' }}>Contact Us</Button>
+            </header>
+        </HeaderContainer>
+    )
+}
+
+export function Header(props: HeaderProps) {
+    const ref = useRef();
+    const [headerFixed, setHeaderFixe] = useState(false);
+
+    useEffect(() => {
+        // @ts-ignore
+        const observer = new IntersectionObserver((entries) => {
+            setHeaderFixe(entries[0].boundingClientRect.bottom < 0)
+        }, { root: null, rootMargin: '0px', threshold: [0, 0.5, 0.8, 1] })
+        console.log(ref.current)
+        observer.observe(ref.current);
+        return () => {
+            if (ref.current) observer.unobserve(ref.current);
+        }
+    }, [])
+
+    return (
+        <>
+            <div ref={ref} >
+                <BaseHeader {...props} />
+            </div>
+            <FixedHeader className='full' style={headerFixed ? {} : { transform: 'translate(0, -100%)' }}>
+                <BaseHeader {...props} />
+            </FixedHeader>
+        </>
+    )
+}
---
+++ packages/ui/components/Logo.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+const CSSLogo = styled.div/*css*/`
+    --background: #ebf5fc;
+
+    --yellow: #f9b446;
+    --pink: #ea2f98;
+
+    --light-blue: #00cdfc;
+    --dark-blue: #007fe8;
+
+    --size: 50px;
+    --tr-time: .5s;
+    margin: 20px 20px 0 0;
+    width: var(--size);
+    height: var(--size);
+    border-radius: 50%;
+    background: linear-gradient(to bottom left, var(--dark-blue) 15%, var(--light-blue));
+    position: relative;
+    cursor: pointer;
+    transition: all var(--tr-time);
+  
+    &::before {
+      content: "";
+      position: absolute;
+      bottom: calc(.4*var(--size));
+      left: calc(.4*var(--size));
+      width: var(--size);
+      height: var(--size);
+      background: linear-gradient(
+        to bottom left,
+        rgba(249, 180, 70, 1) 15%,
+        rgba(234, 47, 152, 0.8)
+      );
+      border-radius: calc(.4*var(--size));
+      border: 4px solid var(--background);
+      transition: all var(--tr-time);
+    }
+    &:hover{
+        margin: 10px 10px 0 0;
+        &:before {
+            bottom: -4px;
+            left: -5px;
+            opacity: .5;
+            transform: scale(.6, .6) rotate(-315deg);
+        }
+    }
+  `
+export function Logo() {
+  return <CSSLogo />
+}
