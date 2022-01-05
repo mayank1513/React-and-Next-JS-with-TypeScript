d9b2e1f3a70cd04e7c233f1982ef92632e40d07c
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-05 21:27:50 +0530

    JSX expressions and comments

---
+++ apps/todo-list/utils/index.ts
@@ -0,0 +1,12 @@
+export const tasks = [
+    {
+        task: 'Task 1',
+        details: '',
+        completed: false
+    },
+    {
+        task: 'Task 2',
+        details: '',
+        completed: false
+    }
+]
