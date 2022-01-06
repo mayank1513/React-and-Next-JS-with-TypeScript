9deb3d77e75d8f881fdfab188d3b93b641db8321
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-06 16:31:07 +0530

    context

--- apps/todo-list/components/TodoList.tsx
+++ apps/todo-list/components/TodoList.tsx
@@ -3,15 +3,17 @@ import { taskType } from "../utils";
 import { TodoListItem } from "./TodoListItem";
 
 
-
-class TodoList extends React.Component<{ tasks: taskType[], title: string }> {
+interface Props { tasks: taskType[], title: string, updateTasks }
+class TodoList extends React.Component<Props> {
     render(): React.ReactNode {
         return (
             <>
                 <h2>{this.props.title}</h2>
                 <ul>
                     {
-                        this.props.tasks.map(task => <TodoListItem key={task.task} task={task} />)
+                        this.props.tasks.map(
+                            task => <TodoListItem updateTasks={this.props.updateTasks} key={task.task} task={task} />
+                        )
                     }
                 </ul>
             </>
