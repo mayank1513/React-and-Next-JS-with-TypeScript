be869f9f9d9eb9d64bff39c174fc603cd4443cde
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-09 18:00:53 +0530

    added-sass

--- apps/todo-list/components/TodoList.tsx
+++ apps/todo-list/components/TodoList.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import { taskType } from "../utils";
 import { TodoListItem } from "./TodoListItem";
 
-
-interface Props { tasks: taskType[], title: string, updateTasks }
+interface Props {
+  tasks: taskType[];
+  title: string;
+  updateTasks;
+}
 class TodoList extends React.Component<Props> {
-    render(): React.ReactNode {
-        return (
-            <>
-                <h2>{this.props.title}</h2>
-                <ul style={{ listStyle: 'none' }}>
-                    {
-                        this.props.tasks.map(
-                            task => <TodoListItem updateTasks={this.props.updateTasks} key={task.task} task={task} />
-                        )
-                    }
-                </ul>
-            </>
-        )
-
-    }
+  render(): React.ReactNode {
+    return (
+      <>
+        <br />
+        <h2>{this.props.title}</h2>
+        <ul style={{ listStyle: "none" }}>
+          {this.props.tasks.map((task) => (
+            <TodoListItem
+              updateTasks={this.props.updateTasks}
+              key={task.task}
+              task={task}
+            />
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
-export default TodoList
+export default TodoList;
--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,60 +1,83 @@
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
 import { useEffect, useRef, useState } from "react";
 
-const Btn = styled.div/*css*/`
---bg-color: red;
---shadow-color: #f004;
---color: white;
-background: var(--bg-color);
-position: relative;
-cursor: pointer;
-height: 50px;
-border-radius: 25px;
-display: inline-flex;
-align-content: center;
-align-items: center;
-padding: 0px 30px;
-margin: 10px;
-box-shadow: 12px 12px 20px 0px var(--shadow-color);
-white-space: nowrap;
-overflow: hidden;
-&, * {
-  font-size: 16px;
-  font-weight: 700;
-  color: var(--color);
-}
-&:hover {
-  box-shadow: none;
-}
-&:before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  transition: all 0.5s;
-  opacity: 1;
-  transform: translate(-105%, 0);
-  background-color: rgba(255, 255, 255, 0.8);
-}
-&:hover:before {
-  opacity: 0;
-  transform: translate(0, 0);
-}
-`
+const Btn = styled.button/*css*/ `
+  all: unset;
+  --bg-color: red;
+  --shadow-color: #f004;
+  --color: white;
+  background: var(--bg-color);
+  position: relative;
+  cursor: pointer;
+  height: 50px;
+  border-radius: 25px;
+  display: inline-flex;
+  align-content: center;
+  align-items: center;
+  padding: 0px 30px;
+  margin: 10px;
+  box-shadow: 12px 12px 20px 0px var(--shadow-color);
+  white-space: nowrap;
+  overflow: hidden;
+  &,
+  * {
+    font-size: 16px;
+    font-weight: 700;
+    color: var(--color);
+  }
+  &:hover {
+    box-shadow: none;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: all 0.5s;
+    opacity: 1;
+    transform: translate(-105%, 0);
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+  &:hover:before {
+    opacity: 0;
+    transform: translate(0, 0);
+  }
+`;
 
 export const Button = ({ children, ...props }: any) => {
   const ref = useRef();
-  const [shadowColor, setShadowColor] = useState('');
+  const [shadowColor, setShadowColor] = useState("");
   useEffect(() => {
-    const bgColor = props.style && (props.style['--bg-color'] || (props.style['background'] && !props.style['background'].includes('url')) || props.style['backgroundColor'] || props.style['background-color']);
-    if (bgColor && !props.style.hasOwnProperty('--shadow-color')) {
-      const bg = window.getComputedStyle(ref.current).backgroundColor.match(/rgba?\((.*)\)/)[1].split(',').map(Number);
-      bg[3] = bg[3] ? .3 * bg[3] : .3;
-      setShadowColor(`rgba(${bg.join(',')})`);
-      console.log(`rgba(${bg.join(',')})`)
+    const bgColor =
+      props.style &&
+      (props.style["--bg-color"] ||
+        (props.style["background"] &&
+          !props.style["background"].includes("url")) ||
+        props.style["backgroundColor"] ||
+        props.style["background-color"]);
+    if (bgColor && !props.style.hasOwnProperty("--shadow-color")) {
+      const bg = window
+        .getComputedStyle(ref.current)
+        .backgroundColor.match(/rgba?\((.*)\)/)[1]
+        .split(",")
+        .map(Number);
+      bg[3] = bg[3] ? 0.3 * bg[3] : 0.3;
+      setShadowColor(`rgba(${bg.join(",")})`);
+      console.log(`rgba(${bg.join(",")})`);
     }
-  }, [props])
-  return <Btn ref={ref} {...props} style={shadowColor.length ? { ...props.style, '--shadow-color': shadowColor } : { ...props.style }}>{children}</Btn>;
+  }, [props]);
+  return (
+    <Btn
+      ref={ref}
+      {...props}
+      style={
+        shadowColor.length
+          ? { ...props.style, "--shadow-color": shadowColor }
+          : { ...props.style }
+      }>
+      {children}
+    </Btn>
+  );
 };
