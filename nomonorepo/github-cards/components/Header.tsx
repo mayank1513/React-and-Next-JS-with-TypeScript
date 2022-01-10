388016c2bdac30649533fe08c218268c788dad10
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-10 13:20:09 +0530

    usestate starting code

--- nomonorepo/github-cards/components/Header.tsx
+++ nomonorepo/github-cards/components/Header.tsx
@@ -1,7 +1,17 @@
+import styles from './Header.module.scss'
+
 export default function Header() {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <header className={styles.header}>
+            <div>
+                <h1>GitHub Cards App</h1>
+                <span className="spacer"></span>
+                <button>
+                    <a href="https://www.udemy.com/user/coding-simplified/" target="_blank" rel="noopener noreferrer">
+                        Take This Course
+                    </a>
+                </button>
+            </div>
+        </header>
     )
 }
