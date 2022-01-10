4d25cf396b44f287780b3db533fa8faa7473b6b8
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-01-10 18:46:37 +0530

    useContext starter code;

--- nomonorepo/github-cards/components/UserInput.tsx
+++ nomonorepo/github-cards/components/UserInput.tsx
@@ -1,14 +1,36 @@
 import styles from './UserInput.module.scss'
+import { profileType } from '../utils';
+import { useRef, useState } from 'react';
 
-export default function UserInput() {
+export default function UserInput({ addCard }: { addCard(newCard: profileType): void }) {
+    // const [state, setstate] = useState(initialState)
+    const myRef = useRef<HTMLInputElement>(null);
+    const [err, setErr] = useState('');
     return (
         <div className={styles.container}>
             <form onSubmit={e => {
                 e.preventDefault();
+                const el = myRef.current as HTMLInputElement
+                fetch(`https://api.github.com/users/${el.value}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        const { name, login, avatar_url, blog, bio, public_gists, public_repos } = data;
+
+                        if (login) {
+                            addCard({ name, login, avatar_url, blog, bio, public_gists, public_repos });
+                            setErr('')
+                        }
+                        else setErr('Profile not found')
+                    }).catch(err => {
+                        setErr(err);
+                    })
             }}>
-                <input type="text" />
+                <input ref={myRef} type="text" />
                 <button>Add Card</button>
             </form>
+            {
+                err && <p className={styles.err}>{err}</p>
+            }
         </div>
     )
 }
