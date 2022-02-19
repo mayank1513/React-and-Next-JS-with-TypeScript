238059d6144be2564d3a00c470810936efb70d63
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-02-19 22:00:46 +0530

    routes-start

--- nomonorepo/github-cards/pages/index.tsx
+++ nomonorepo/github-cards/pages/index.tsx
@@ -6,6 +6,8 @@ import styles from '../styles/Home.module.scss'
 import { defaultProfiles } from '../utils'
 import { profileType } from '../utils'
 
+import Head from 'next/head'
+
 import { useState, useContext, createContext } from 'react'
 
 type GlobalProfileContextTypes = [profileType[], (p: profileType[]) => void]
@@ -19,6 +21,15 @@ const Home: NextPage = () => {
 
   return (
     <ProfilesContext.Provider value={[profiles, setProfiles]}>
+      <Head>
+        <meta name="description" content="This is a sample app used to teach React JS functional componets and hooks." />
+        <title>GitHub Cards App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+      </Head>
       <div>
         <Header />
         <UserInput />
