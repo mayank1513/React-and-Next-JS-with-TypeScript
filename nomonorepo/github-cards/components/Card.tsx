87ed5f6b8efbbe2e6909ea015edbf4195cd4cfa0
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-03-20 13:25:33 +0530

    nested dynamic routes

--- nomonorepo/github-cards/components/Card.tsx
+++ nomonorepo/github-cards/components/Card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { profileType } from "../utils"
 import styles from './Card.module.scss'
 
@@ -5,21 +6,23 @@ interface CardPropType { profile: profileType }
 
 export default function Card({ profile }: CardPropType) {
     return (
-        <div className={styles.container}>
-            <div>
-                <img src={profile.avatar_url} alt="Avatar" />
+        <Link href={profile.login}>
+            <div className={styles.container}>
+                <div>
+                    <img src={profile.avatar_url} alt="Avatar" />
+                </div>
+                <div>
+                    <h2>{profile.name} ({profile.login})</h2>
+                    <p>{profile.bio}</p>
+                    <p>
+                        {
+                            profile.blog && <a href={profile.blog} target="_blank" rel="noopener noreferrer">Website</a>
+                        }
+                        <code>Public Repositories: {profile.public_repos}</code>
+                        <code>Public Gists: {profile.public_gists}</code>
+                    </p>
+                </div>
             </div>
-            <div>
-                <h2>{profile.name} ({profile.login})</h2>
-                <p>{profile.bio}</p>
-                <p>
-                    {
-                        profile.blog && <a href={profile.blog} target="_blank" rel="noopener noreferrer">Website</a>
-                    }
-                    <code>Public Repositories: {profile.public_repos}</code>
-                    <code>Public Gists: {profile.public_gists}</code>
-                </p>
-            </div>
-        </div>
+        </Link>
     )
 }
