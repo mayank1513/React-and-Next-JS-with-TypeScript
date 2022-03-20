e6916a32750f2c68ada23d2e8f31de303ad9460e
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-03-20 13:25:33 +0530

    nested dynamic routes

--- nomonorepo/github-cards/pages/[login_id].tsx
+++ nomonorepo/github-cards/pages/[login_id].tsx
@@ -1,10 +1,39 @@
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
 
+type RepoType = { name: string, description: string, html_url: string, default_branch: string }
 export default function() {
     const router = useRouter();
-    return (
-        <div style={{padding: '10px'}}>
-            {router.query.login_id}
+    const [repos, setRepos] = useState<RepoType[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await fetch(`https://api.github.com/users/${router?.query?.login_id}/repos`);
+                const data = await result.json();
+                setRepos(data.map(({ name, description, html_url, default_branch }: RepoType) => ({
+                    name, description, html_url, default_branch
+                })))
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+    }, [router?.query?.login_id])
+
+    return repos?.length ? (
+        <ul className="container">
+            {
+                repos?.map(repo => (<li>
+                    <p>
+                        <h3>{repo.name}</h3>
+                        {repo.description}
+                    </p>
+                </li>))
+            }
+        </ul>
+    ) : (
+        <div className="container">
+            Did not find any Repo here!
         </div>
     )
 }
