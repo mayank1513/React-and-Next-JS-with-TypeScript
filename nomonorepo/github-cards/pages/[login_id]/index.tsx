76b6da5ad86ff4c67f68db52618114cd4eb77673
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-03-22 13:56:07 +0530

    ReactMarkdown components

--- nomonorepo/github-cards/pages/[login_id]/index.tsx
+++ nomonorepo/github-cards/pages/[login_id]/index.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 
 type RepoType = { name: string, description: string, html_url: string, default_branch: string }
-export default function() {
+export default function () {
     const router = useRouter();
     const [repos, setRepos] = useState<RepoType[]>([]);
+    const { login_id } = router?.query;
 
     useEffect(() => {
         (async () => {
             try {
-                const result = await fetch(`https://api.github.com/users/${router?.query?.login_id}/repos`);
+                const result = await fetch(`https://api.github.com/users/${login_id}/repos`);
                 const data = await result.json();
                 setRepos(data.map(({ name, description, html_url, default_branch }: RepoType) => ({
                     name, description, html_url, default_branch
@@ -18,17 +20,19 @@ export default function() {
                 console.log(error)
             }
         })()
-    }, [router?.query?.login_id])
+    }, [login_id])
 
     return repos?.length ? (
         <ul className="container">
             {
-                repos?.map(repo => (<li>
-                    <p>
-                        <h3>{repo.name}</h3>
-                        {repo.description}
-                    </p>
-                </li>))
+                repos?.map(({ name, description, html_url, default_branch }) => (
+                    <Link href={`${login_id}/${name}/${default_branch}`}>
+                        <li>
+                            <p>
+                                <h3>{name}</h3>
+                                {description}
+                            </p>
+                        </li></Link>))
             }
         </ul>
     ) : (
