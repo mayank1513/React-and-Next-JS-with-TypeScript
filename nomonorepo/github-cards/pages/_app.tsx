b1eb362e3341f2f84fe562f410ba6b9eb86d9b99
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-03-23 11:28:06 +0530

    static assets done

--- nomonorepo/github-cards/pages/_app.tsx
+++ nomonorepo/github-cards/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="github-logo.png" type="image/x-icon" />
+      </Head>
         <Header />
         <Component {...pageProps} />
     </>
