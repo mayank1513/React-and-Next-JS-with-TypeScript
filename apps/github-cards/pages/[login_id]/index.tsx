cab19286572221c6bc8e68969733619eaa0f7dba
Mayank Chaudhari <mayank.srmu@gmail.com>
2022-06-18 22:10:37 +0530

    move github cards to monorepo

---
+++ apps/github-cards/pages/[login_id]/index.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
+
+type RepoType = { name: string, description: string, html_url: string, default_branch: string }
+export default function () {
+    const router = useRouter();
+    const [repos, setRepos] = useState<RepoType[]>([]);
+    const { login_id } = router?.query;
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await fetch(`https://api.github.com/users/${login_id}/repos`);
+                const data = await result.json();
+                setRepos(data.map(({ name, description, html_url, default_branch }: RepoType) => ({
+                    name, description, html_url, default_branch
+                })))
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+    }, [login_id])
+
+    return repos?.length ? (
+        <ul className="container">
+            {
+                repos?.map(({ name, description, html_url, default_branch }) => (
+                    <Link href={`${login_id}/${name}/${default_branch}`}>
+                        <li className="link">
+                            <p>
+                                <h3>{name}</h3>
+                                {description}
+                            </p>
+                        </li></Link>))
+            }
+        </ul>
+    ) : (
+        <div className="container">
+            Did not find any Repo here!
+        </div>
+    )
+}
