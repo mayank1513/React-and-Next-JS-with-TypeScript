0d422b3b68c39445136f7d946fc5311c85f3c609
Mayank <mayank.srmu@gmail.com>
2022-07-14 16:43:41 +0000

    image optimization

--- apps/portfolio/components/Background.tsx
+++ apps/portfolio/components/Background.tsx
@@ -1,26 +1,33 @@
 import { styled } from "linaria/react";
 import Image from "next/image";
 import stars from "/assets/stars.jpg";
-import webDev from "/assets/web-dev.gif";
-import computerVision1 from "/assets/cv.gif";
-import computerVision2 from "/assets/cv1.gif";
-import fullStack from "/assets/full-stack.gif";
-import mobile from "/assets/mobile.gif";
 import research from "/assets/research.jpg";
-import prof from "/assets/prof.gif";
 import edu from "/assets/edu.jpg";
-import yogi from "/assets/yogi.gif";
 
 const StyledBackground = styled.div`
   &,
   .bg {
     position: fixed;
-    display: block;
+    display: flex;
+    align-item: center;
+    justify-content: center;
     z-index: -100;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
+    video {
+      &.cover {
+        position: fixed;
+        min-width: 100%;
+        min-height: 100%;
+      }
+      &.contain {
+        max-width: 100%;
+        max-height: 100%;
+        z-index: 10;
+      }
+    }
   }
   .cover {
     filter: blur(4px);
@@ -41,16 +48,35 @@ export default function Background({ bg }) {
     <StyledBackground>
       {[
         stars,
-        webDev,
-        computerVision1,
-        fullStack,
-        computerVision2,
-        mobile,
+        "web-dev",
+        "cv",
+        "full-stack",
+        "cv1",
+        "mobile",
         research,
-        prof,
+        "prof",
         edu,
-        yogi,
+        "yogi",
       ].map((src) => {
+        if (typeof src == "string") {
+          const cls = src;
+          return (
+            <div
+              key={cls}
+              className={`bg ${cls}`}
+              style={{ opacity: bg == cls ? 1 : 0 }}
+            >
+              <video autoPlay loop muted playsInline className="cover">
+                <source src={`/${cls}.webm`} type="video/webm" />
+                <source src={`/${cls}.mp4`} type="video/mp4" />
+              </video>
+              <video autoPlay loop muted playsInline className="contain">
+                <source src={`/${cls}.webm`} type="video/webm" />
+                <source src={`/${cls}.mp4`} type="video/mp4" />
+              </video>
+            </div>
+          );
+        }
         const cls = src.src.slice(src.src.lastIndexOf("/") + 1).split(".")[0];
         return (
           <div
