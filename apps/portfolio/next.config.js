cafdcc5c8634ad21f7828cccc87bfd717f0ce25a
Mayank <mayank.srmu@gmail.com>
2022-07-15 12:39:48 +0000

    Update Reame

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -2,11 +2,12 @@ const withTM = require("next-transpile-modules")(["ui"]);
 const withLinaria = require("next-linaria");
 
 module.exports = withLinaria(
-    withTM({
-        reactStrictMode: true,
-        images: {
-            loader: 'akamai',
-            path: '',
-        },
-    })
+  withTM({
+    reactStrictMode: true,
+    images: {
+      format: ["image/webp", "image/png", "image/jpeg"],
+      loader: "akamai",
+      path: "",
+    },
+  })
 );
